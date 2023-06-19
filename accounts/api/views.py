54343ed6754e29e17821eb1312bca4b7268e5b12
pemidi <rashidi.peyman@gmail.com>
2023-06-20 01:54:40 +0330

    add: Add TODOs

---
+++ accounts/api/views.py
@@ -0,0 +1,10 @@
+# TODO: Add a Register API for customer, the customer can Register with username and password
+
+
+# TODO: add a Register API for restaurant, the restaurant can Register with username and password
+
+
+# TODO: add a Login API to login Customer and Restaurant by username and password.
+
+
+# TIP: DO NOT USER PHONE_NUMBER FOR ANY OF REGISTER OR LOGIN TASKS
