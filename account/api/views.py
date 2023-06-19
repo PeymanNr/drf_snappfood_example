1c0f95ffe94172eb8294d0297c2e3f3f8fb1caeb
PeymanNr <peyman.naseri7@gmail.com>
2023-06-19 21:23:33 +0330

    Added Views Login & Register User

--- account/api/views.py
+++ account/api/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
-from account.api.serializers import UserRegisterSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from account.api.serializers import UserRegisterSerializer, UserLoginSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
 
-class UserRegistrationCreateAPIView(CreateAPIView):
-    serializer_class = UserRegisterSerializer
+class UserRegistrationCreateAPIView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'user': serializer.data,
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginAPIView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'user': serializer.data,
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
