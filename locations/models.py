4f77ebf5452170e504c650a3014538800809304d
pemidi <rashidi.peyman@gmail.com>
2023-06-20 01:22:22 +0330

    add: add models of location app

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+from utils.base_models import BaseModel
+
+
+class Country(BaseModel):
+    name = models.CharField(max_length=32, verbose_name=_('country'))
+
+    # TODO: add meta class
+
+
+class City(BaseModel):
+    name = models.CharField(max_length=32, verbose_name=_('city'))
+    country = models.ForeignKey(Country, verbose_name=_('country'), related_name='cities', on_delete=models.CASCADE)
+
+    # TODO : add meta class
+
+
+class Address(BaseModel):
+    city = models.ForeignKey(City, verbose_name=_('city'), related_name='addresses', on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, verbose_name='description')
+
+    # TODO: add meta class
