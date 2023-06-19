2d7da6286710e2bbe0fbfa2e74c2996aa831ac6a
pemidi <rashidi.peyman@gmail.com>
2023-06-20 01:36:52 +0330

    refactor: refactor accounts app, refactor models, remove views and admin

--- accounts/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts.api.views import UserRegistrationCreateAPIView, UserLoginAPIView
-
-urlpatterns = [
-    path('register/', UserRegistrationCreateAPIView.as_view(), name='user-register'),
-    path('login/', UserLoginAPIView.as_view(), name='user-login'),
-
-]
--- accounts/models.py
+++ accounts/models.py
@@ -1,61 +1,29 @@
-from django.core.validators import RegexValidator
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext as _
+
+from locations.models import Address
+from utils.base_models import BaseModel
+
+User = get_user_model()
 
 
-class MyUserManager(BaseUserManager):
-    use_in_migrations = True
+class Restaurant(BaseModel):
+    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, verbose_name=_('name'))
+    address = models.ForeignKey(Address, verbose_name='address', on_delete=models.SET_NULL, null=True)
 
-    def create_user(self, phone_number, password=None, **extra_fields):
-        """Creates and saves user with given phone and password"""
-        if not phone_number:
-            raise ValueError('phone number required!')
-        user = self.model(phone_number=phone_number, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, phone_number, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        return self.create_user(phone_number, password, **extra_fields)
+    # TODO: add meta class
+    # TODO: add __str__
 
 
-class MyUser(AbstractUser):
-    username = None
-    phone_regex = RegexValidator(
-        regex=r'^(\+98|0)?9\d{9}$',
-        message="Phone number must be entered in the format: '09...' or format: '+989..."
-    )
-    phone_number = models.CharField(validators=[phone_regex], verbose_name='Mobile Number', unique=True,
-                                    max_length=13)
-    nickname = models.CharField(max_length=10)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    objects = MyUserManager()
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-    USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = []
+class Customer(BaseModel):
+    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, verbose_name=_('first_name'))
+    last_name = models.CharField(max_length=32, verbose_name=_('last name'))
+    # TODO: add validator to validate the phone number of user
+    phone = models.CharField(max_length=32)
+    address = models.ForeignKey(Address, verbose_name=_('address'), on_delete=models.SET_NULL, null=True)
 
-    def __str__(self):
-        return self.phone_number
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-        db_table = 'users'
-
-
-class Seller(models.Model):
-    seller = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    shop_name = models.CharField(max_length=32)
-    vendor_first_name = models.CharField(max_length=32)
-    vendor_last_name = models.CharField(max_length=32)
-    phone_number = models.CharField(verbose_name='Mobile Number', unique=True, max_length=13)
-
+    # TODO: add meta class
+    # TODO: add __str__
