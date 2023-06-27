01548d48337f837a69f6a1cf5dbfac404cfa8386
pemidi <rashidi.peyman@gmail.com>
2023-06-27 13:05:09 +0330

    add: add restaurants app

--- snappfood/settings.py
+++ snappfood/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+
     'accounts',
     'locations',
+    'restaurants'
 ]
 
 MIDDLEWARE = [
