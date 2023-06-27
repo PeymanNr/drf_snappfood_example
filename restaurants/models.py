eac2de42eb750aa671e07fbb48c6bdab4df829a4
pemidi <rashidi.peyman@gmail.com>
2023-06-27 13:07:02 +0330

    add: add restaurants app tasks

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+#TODO: Add Models for restaurants app like Resturant model. menu, menu item, etc
+
+#TODO: add views to show the all restaurants by city and other parameters.
+
