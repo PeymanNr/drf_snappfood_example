5fdcb7adc0a9fbc287053557b804f79b8fddc3b0
pemidi <rashidi.peyman@gmail.com>
2023-07-05 17:45:01 +0330

    add: add tasks

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# TODO: add order model and other related models.
+# TODO: add views for show all orders for customers and restaurants.
+# TODO: security tips: don't get user from PK. get user from request. show the order by type of the user
+# TODO: this is an example for endpoints: orders/1
+
